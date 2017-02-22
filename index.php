369a5a214819dcdfcd055588000820c7ca578af6
ghansum <ghansumn@gmail.com>
2017-02-22 12:28:40 +0100

    up

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html >
   <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5T78FK7');</script>
+<!-- End Google Tag Manager -->
+
     <meta charset="UTF-8">
     <title>Twitter Dataviz</title>
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
@@ -23,7 +31,10 @@
   </head>
   
   <body>
- 
+    <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5T78FK7"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
     <div class="fixed-div">
         <div class="col-md-12 text-center">
           <img src="media/Twitter_logo.png" height="50px">
